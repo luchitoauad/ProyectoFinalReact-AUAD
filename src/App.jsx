b61c1f6fa820d4f13d7f110aee00d48bcb2bb6d0
luchitoauad <auad.luciano@gmail.com>
2023-09-04 22:03:40 -0300

    formulario de compra agregado

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailCont
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
 import Cart from './components/Cart/Cart'
-
+import Checkout from './components/Checkout/Checkout'
 
 function App() {
 
@@ -25,6 +25,7 @@ function App() {
             <Route path='/category/:categoryId' element={<ItemListContainer greeting={'Bienvenidos a MotoStar'} />}/>
             <Route path='/item/:itemId' element={ <ItemDetailContainer />}/>
             <Route path='/cart' element={ <Cart/>}/>
+            <Route path='/checkout' element={ <Checkout/>}/>
             <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
           </Routes>
           </CartProvider>
